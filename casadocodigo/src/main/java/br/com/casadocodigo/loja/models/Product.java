0a693811cf42e624267b66a0f5fae9f544efaaea
Rafael Hiroshi <hiroshi_nuts@hotmail.com>
2016-08-03 16:04:15 -0300

    ModelandView , Listagem, disponivel na tela, redirect, forward , Protocolos de requisicao (GET,POST), FiM capitulo 4

--- casadocodigo/src/main/java/br/com/casadocodigo/loja/models/Product.java
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/models/Product.java
@@ -1,5 +1,9 @@
 package br.com.casadocodigo.loja.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -16,6 +20,17 @@ public class Product {
 	@Lob
 	private String description;
 	private Integer pages;
+	@ElementCollection
+	private List<Price> prices = new ArrayList<Price>();
+	
+	
+	
+	public List<Price> getPrices() {
+		return prices;
+	}
+	public void setPrices(List<Price> prices) {
+		this.prices = prices;
+	}
 	public Integer getId() {
 		return id;
 	}
